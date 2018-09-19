166dc5f9e7be0f49b5807753bbd5df9d24b57ac8
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-09-19 11:05:40 -0400

    This is when I wrote the code not sure if it's accurate yet.

---
+++ CtoFTester.java
@@ -0,0 +1,14 @@
+public static double celsiusToFahrenheit (double C) {
+
+(C * (9 / 5))  + 32 == sum;
+
+return sum;
+}
+
+public status double fahrenheitToCelsius (double F) {
+
+((F - 32) * (5 / 9)) == sum;
+
+return sum;
+
+}
