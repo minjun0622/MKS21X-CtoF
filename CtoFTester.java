2dfd95803b143f6ec941cd568b03cac8c48d6693
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-09-19 21:02:52 -0400

    This is a new commit because I fixed the code! It has been edited and tested and it passes the qualifications.

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,14 +1,25 @@
-public static double celsiusToFahrenheit (double C) {
+public class CtoFTester {
 
-(C * (9 / 5))  + 32 == sum;
+  public static void main (String [] args) {
 
-return sum;
+  System.out.println (celciusToFahrenheit (0.0 )); // 32.0 degrees! Simple!
+  System.out.println (celciusToFahrenheit (24.2)); // 75.56 degrees Fahrenheit, Yay!
+
+  System.out.println (fahrenheitToCelcius (32.0)); // 0.0 degrees!
+  System.out.println (fahrenheitToCelcius (212.0)); // 100.0 degrees!
+
+
+  }
+
+  public static double celciusToFahrenheit (double C) {
+
+return ( (C * (9.0 / 5) ) + 32) ;
 }
+// This function is for Celcius to Fahrenheit!
 
-public status double fahrenheitToCelsius (double F) {
+public static double fahrenheitToCelcius(double F) {
 
-((F - 32) * (5 / 9)) == sum;
-
-return sum;
-
+return ( (F - 32) * (5.0 / 9) ) ;
+}
+// This function is for Fahrenheit to Celcius!
 }
